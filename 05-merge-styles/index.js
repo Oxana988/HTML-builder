621630e1fc97fd26720ff97da01fee3ed9d8dd6f
Oxana Savianok <Oxana8833@gmail.com>
2022-11-07 23:20:35 +0300

    feat: add solution for 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+//const pathToFolder = path.join(__dirname, 'project-dist');
+const pathToSourceFolder = path.join(__dirname, 'styles');
+const pathToFile = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+
+fs.readdir(pathToSourceFolder, {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+        if (path.extname(file.name) === '.css') {
+            const pathToSourceFile = fs.createReadStream(path.join(__dirname, 'styles', file.name), 'utf-8');
+            pathToSourceFile.on('data', chunk => pathToFile.write(chunk));
+          }
+    }
+})
