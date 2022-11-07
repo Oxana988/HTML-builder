f0f75effba434d80e10b50ffbd0ad89fe1717535
Oxana Savianok <Oxana8833@gmail.com>
2022-11-07 22:14:16 +0300

    feat: add solution for 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,12 +6,12 @@ fs.readdir(pathToFolder, {withFileTypes: true}, (err, files) => {
     if (err) throw err;
     for (let file of files) {
         if (file.isFile()) {
-            fs.stat(pathToFolder, file.name, (err, stats) => {
+            fs.stat((path.join(__dirname, 'secret-folder', file.name)), (err, stats) => {
                 if (err) throw err;
                 //console.log(path.parse(file.name).name);
                 //console.log(path.extname(file.name).substring(1));
-                console.log(stats.size);
-                console.log(path.parse(file.name).name + ' - ' + path.extname(file.name).substring(1) + ' - ' + stats.size + ' b');
+                //console.log(stats.size);
+                console.log(path.parse(file.name).name + ' - ' + path.extname(file.name).substring(1) + ' - ' + stats.size + 'b');
             })
         } 
     }
