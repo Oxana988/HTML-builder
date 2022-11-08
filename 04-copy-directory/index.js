da8d7f1929a8c6520ebe739ff15474728d9b2be6
Oxana Savianok <Oxana8833@gmail.com>
2022-11-08 14:06:14 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const pathToFolder = path.join(__dirname, 'files-copy');
-const pathToSourceFolder = path.join(__dirname, 'files');
+//const pathToSourceFolder = path.join(__dirname, 'files');
 
 fs.mkdir(pathToFolder, () => {
-  fs.readdir(pathToFolder, (err, files) => {
+  fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
     if (err) throw err;
     for (let file of files) {
         fs.unlink(path.join(__dirname, 'files-copy', file), (err) => { 
@@ -12,7 +12,7 @@ fs.mkdir(pathToFolder, () => {
     })
     }
   });
-  fs.readdir(pathToSourceFolder, (err, files) => {
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
     if (err) throw err;
     for (let file of files) {
         fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), () => { });
